1f21450698d395f81c626709099ec697a1b6b570
유지현 <jihyunn@yujihyeon-ui-MacBookPro.local>
2025-10-17 16:06:34 +0900

    [Feature] 메모 기능 구현
    1. 모델 구현
       - Memo 모델 생성 (제목, 내용, 작성일, 수정일, 작성자)
       - 정렬 및 메타 정보 설정
    
    2. 폼 구현
       - MemoForm 생성 (제목, 내용 필드)
       - Bootstrap 스타일 적용
    
    3. 뷰 구현
       - 메모 목록 보기 (memo_list)
       - 메모 작성 (memo_create)
       - 메모 상세 보기 (memo_detail)
       - 메모 수정 (memo_update)
       - 메모 삭제 (memo_delete)
    
    4. 템플릿 구현
       - 메모 목록 페이지
       - 메모 상세 페이지
       - 메모 작성/수정 폼
       - 메모 삭제 확인 페이지
    
    5. URL 패턴 설정
    6. 데이터베이스 마이그레이션 완료

--- memos/models.py
+++ memos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Memo(models.Model):
+    title = models.CharField(max_length=200, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memos', verbose_name='작성자')
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = '메모'
+        verbose_name_plural = '메모들'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('memo_detail', kwargs={'pk': self.pk})
--- memos/views.py
+++ memos/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Memo
+from .forms import MemoForm
 
 def memo_list(request):
-    return render(request, 'memos/memo_list.html')
+    memos = Memo.objects.all()
+    return render(request, 'memos/memo_list.html', {'memos': memos})
+
+@login_required
+def memo_create(request):
+    if request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            memo = form.save(commit=False)
+            memo.author = request.user
+            memo.save()
+            messages.success(request, '메모가 생성되었습니다.')
+            return redirect('memo_detail', pk=memo.pk)
+    else:
+        form = MemoForm()
+    return render(request, 'memos/memo_form.html', {'form': form})
+
+def memo_detail(request, pk):
+    memo = get_object_or_404(Memo, pk=pk)
+    return render(request, 'memos/memo_detail.html', {'memo': memo})
+
+@login_required
+def memo_update(request, pk):
+    memo = get_object_or_404(Memo, pk=pk)
+    if memo.author != request.user:
+        messages.error(request, '다른 사용자의 메모는 수정할 수 없습니다.')
+        return redirect('memo_detail', pk=pk)
+    
+    if request.method == 'POST':
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '메모가 수정되었습니다.')
+            return redirect('memo_detail', pk=pk)
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'memos/memo_form.html', {'form': form})
+
+@login_required
+def memo_delete(request, pk):
+    memo = get_object_or_404(Memo, pk=pk)
+    if memo.author != request.user:
+        messages.error(request, '다른 사용자의 메모는 삭제할 수 없습니다.')
+        return redirect('memo_detail', pk=pk)
+    
+    if request.method == 'POST':
+        memo.delete()
+        messages.success(request, '메모가 삭제되었습니다.')
+        return redirect('home')
+    return render(request, 'memos/memo_confirm_delete.html', {'memo': memo})
